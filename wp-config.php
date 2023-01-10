67ea8c0134e03956c7bc81ad3c27f6f95134118d
Armin Djulovic <armindjulovic@Armins-MacBook-Air-2.local>
2023-01-10 14:26:40 +0100

    finished functionality

--- wp-config.php
+++ wp-config.php
@@ -79,9 +79,9 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-define( 'WP_DISABLE_FATAL_ERROR_HANDLER', true ); // 5.2 and later
-define( 'WP_DEBUG', true );
-define( 'WP_DEBUG_LOG', true );
+define( 'WP_DISABLE_FATAL_ERROR_HANDLER', false ); // 5.2 and later
+define( 'WP_DEBUG', false );
+define( 'WP_DEBUG_LOG', false );
 
 
 /* Add any custom values between this line and the "stop editing" line. */
