e981fbca93a06b6501e6baf47cd7a8b40bd01c15
Armin Djulovic <armindjulovic@Armins-MacBook-Air-2.local>
2023-01-14 13:17:28 +0100

    project is done

--- wp-content/themes/armin_wp_tema/archive-latest.php
+++ wp-content/themes/armin_wp_tema/archive-latest.php
@@ -13,40 +13,52 @@ get_header();
 
 
 <?php 
-  $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-  $args = [
+
+  $get_query_var = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
+  $latest = [
     'category_name' => 'latest',
     'orderby'        => 'date',
     'order'          => 'asc',
-    'paged' => $paged,
+    'post_per_page' => 3,
+    'paged' => $get_query_var,
   ];
-  $custom_query = new WP_Query($args);
-  if ($custom_query->have_posts()) {
-    while ($custom_query->have_posts()) {
-      $custom_query->the_post();
-    ?>  
+
+  $query = new WP_Query($latest);
+
+  if ($query->have_posts()) {
+    while ($query->have_posts()) {
+      $query->the_post();
+    ?>
+    
     <div class="archive_card">
+
       <a class="archive_read_more_link" href="<?php the_permalink(); ?>">
+
         <?php 
         if ( has_post_thumbnail() ) { 
           the_post_thumbnail('medium_large', [ 'class' => 'latest_archive_thumbnail' ]); 
         }
         ?>
+
         <h2 class="read_more_archive"><?php the_title(); ?></h2></a>
+
     </div>
+
     <?php
     }
   }
 
   ?>
 
-    <div>
+    <div class="pagination_container">
         <?php
-    echo paginate_links( [
-        'total' => $custom_query->max_num_pages,
-        'prev_text' => 'Previous',
-        'next_text' => 'Next',
-        ] );
+        echo paginate_links( [
+            'total' => $query->max_num_pages,
+            'prev_text' => 'Previous',
+            'next_text' => 'Next',
+            ] );
+            
         wp_reset_postdata();
         ?>
 
@@ -60,13 +72,11 @@ get_footer();
 
 /* Gammal kod. Funkade inte pga query_posts funktionen. Den påverkar huvud queryn för sidan och laddar istället in samma posts och gör att
 min pagination inte funkar... Istället får vi använda oss av WP_Qeury klassen för att skapa en ny query för posterna 
-och sedan paginera detta med en echo paginate_links". Vi använder också wp_reset_postdata(9 efter loopen för att den ska gå tillbaka till
-sin orginala query.)
-
+och sedan paginera detta med en "echo paginate_links". Vi använder också wp_reset_postdata() efter loopen för att den ska gå tillbaka till
+sin orginala query.
 */
 
 /* 
-
 query_posts( [ 
     'category_name' => 'latest',
     'orderby'        => 'date',
